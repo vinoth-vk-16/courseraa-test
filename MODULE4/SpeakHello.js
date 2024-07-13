67e96456374217676fc76ba8bb4d05a2d440a904
vinoth-vk-16 <127669795+vinoth-vk-16@users.noreply.github.com>
2024-07-13 12:20:08 +0530

    deploy

---
+++ MODULE4/SpeakHello.js
@@ -0,0 +1,9 @@
+(function(window){
+    var helloSpeaker ={
+        speak: function (name) {
+            console.log(speakWord + " " + name);
+          }
+    };
+    var speakWord = "Hello";
+    window.helloSpeaker=helloSpeaker;
+})(window);
